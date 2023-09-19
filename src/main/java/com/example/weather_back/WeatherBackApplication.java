067a3df8611abfc49a28f445b59ff5d88699e822
rasmusvark <rasmusvark@gmail.com>
2023-09-19 06:07:05 +0300

    Initial commit

---
+++ src/main/java/com/example/weather_back/WeatherBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.weather_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/weather_back/WeatherBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weather_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
